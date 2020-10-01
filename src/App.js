cecc53bd2726219c5b7bee34f4ce56325db6da36
DianaMuljukova <pavlenkodr@yandex.ru>
2020-10-01 21:01:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
+import _ from "lodash";
+import {v4} from "uuid";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setState]= useState({});
+    const [text, setText]= useState('');
+
+    useEffect(() => {
+        getData()
+    }, []);
+
+    const getData = async () => {
+        const response = await fetch("/data.json");
+        const result = await response.json();
+        setState(result);
+    };
+
+    const handleDragEnd = ({destination, source}) => {
+        if (!destination || (destination.index === source.index && destination.droppableId === source.droppableId)) {
+            return
+        }
+        const itemCopy = state[source.droppableId].items[source.index];
+        setState(prev => {
+            prev = {...prev};
+            prev[source.droppableId].items.splice(source.index, 1);
+            prev[destination.droppableId].items.splice(destination.index, 0, itemCopy);
+            return prev;
+        })
+    };
+
+    const addItem = () => {
+        setState(prev => {
+            return {
+                ...prev,
+                todo: {
+                    title: "Todo",
+                    items: [{
+                        id: v4(),
+                        name: text
+                    }, ...prev.todo.items]
+                }
+            }
+        })
+
+        setText("")
+    };
+
+
+    return (
+        <>
+        <div className="add_block">
+            <input type="text" value={text} onChange={e => setText(e.target.value)}/>
+            <button onClick={addItem}>Add</button>
+        </div>
+        <div className="App">
+            <DragDropContext onDragEnd={handleDragEnd}>
+                {
+                    _.map(state, (data, key) => {
+                        return (
+                            <div className="column" key={key}>
+                                <h3>{data.title}</h3>
+                                <Droppable droppableId={key}>
+                                    {
+                                        provided => {
+                                            return (
+                                                <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.droppableProps}
+                                                    className="droppable-col"
+                                                >
+                                                    {data.items.map((el, i) => {
+                                                        return (
+                                                            <Draggable key={el.id} index={i} draggableId={el.id}>
+                                                                {
+                                                                    provided => {
+                                                                        return (
+                                                                            <div
+                                                                                className="item"
+                                                                                ref={provided.innerRef}
+                                                                                {...provided.draggableProps}
+                                                                                {...provided.dragHandleProps}>
+                                                                                { el.name }
+                                                                            </div>
+                                                                        )
+                                                                    }
+                                                                }
+                                                            </Draggable>
+                                                        )
+                                                    })}
+                                                    {provided.placeholder}
+                                                </div>
+                                            )
+                                        }
+                                    }
+                                </Droppable>
+                            </div>
+                        )
+                    })
+                }
+            </DragDropContext>
+        </div>
+            </>
+    );
 }
 
 export default App;
